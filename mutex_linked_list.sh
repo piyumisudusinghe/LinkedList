86b89d50c7f4f30fbf5544c56fc418cb29ac1c46
supimi <supimi.15@cse.mrt.ac.lk>
2019-04-05 09:44:14 +0530

    modify to get arguments by shell

--- mutex_linked_list.sh
+++ mutex_linked_list.sh
@@ -2,8 +2,8 @@
 
 gcc -g -Wall -o mutex_linked_list mutex_linked_list.c -lpthread
 
- for i in `seq 1 $1`;
+ for i in `seq 1 $7`;
         do
                 echo "Number of times: " $i
-                ./mutex_linked_list 1000 10000 1 0.99 0.005 0.005
+                ./mutex_linked_list $1 $2 $3 $4 $5 $6
         done   
