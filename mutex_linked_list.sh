f83a4d1671a49ea65bfafd0c601e1bb9d724cbd6
piyumi <piyumisudusinghe@gmail.com>
2019-04-05 15:06:44 +0530

    Delete some files

--- mutex_linked_list.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gcc -g -Wall -o mutex_linked_list mutex_linked_list.c -lpthread
-
- for i in `seq 1 $7`;
-        do
-                echo "Number of times: " $i
-                ./mutex_linked_list $1 $2 $3 $4 $5 $6
-        done   
